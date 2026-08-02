34affabb4b0097909377f458e87bf9d79b25957c
mevikash13195 <vksingh13195@gmail.com>
2026-08-02 20:25:40 +0530

    PR

--- SeleniumFramework/src/test/java/learning/SeleniumFramework/stepDefinitions/ui/LoginUISteps.java
+++ SeleniumFramework/src/test/java/learning/SeleniumFramework/stepDefinitions/ui/LoginUISteps.java
@@ -25,6 +25,7 @@ public class LoginUISteps {
 	@Given("I launch the application in browser")
 	public void i_launch_the_application_in_browser(){
 		loginPage.launchApp();
+		//Learning PR
 		
 	}
 	
